bceda9ee14eb18c66ffe491d7f61747c02a3fa19
yanamalaumesh1874 <62681339+yanamalaumesh1874@users.noreply.github.com>
2020-04-06 20:58:25 +0530

    Create scannerm2.java

---
+++ scannerm2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class method2
+{
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		System.out.println("enter a,b values");
+		a=(new Scanner(System.in)).nextInt();
+		b=(new Scanner(System.in)).nextInt();
+		c=a+b;
+		System.out.println("sum is "+c);
+	}
+}
